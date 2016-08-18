1c5008bcf634540ae80ee56b695900c0913dab2e
Thomas Hegge <thegge@gmail.com>
2016-08-18 20:07:18 +0200

    Some enhancements Testing progress indicator in header

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ var App = {
       switch (obj.type) {
         case "article":
         case "report":
-          ArticleView.init(document.getElementById('article'));
+          ArticleView.init(document.getElementById('article'), obj);
           break;
         default:
 
@@ -20,7 +20,10 @@ var App = {
     Header.init(document.getElementById('site-header'));
     var throttled = _.throttle(_.bind(Header.hideMenu, Header), 500);
     var delta = 0;
+    var height = $(window).height();
     var prev = window.scrollY;
+    var $hsp = $('.header-scroll-progress');
+    var $article = $('article');
     $(window).on('scroll', function(e) {
       delta += (prev - window.scrollY);
       prev = window.scrollY;
@@ -29,6 +32,11 @@ var App = {
         throttled();
         delta = 0;
       }
+
+      if(obj.type && (obj.type === "article" || obj.type === "report")) {
+        $hsp.width(Math.min(($(window).scrollTop()/($article.height() - $(window).height())*100), 100)+"%");
+      }
+
     });
   }
 }
--- src/js/views/article.js
+++ src/js/views/article.js
@@ -1,15 +1,30 @@
 var $ = require('jquery');
+var _ = require('lodash');
+
+
 var ArticleRelatedContent = require('./article-relatedcontent');
 
 var ArticleView = {
-  init: function (el) {
+  init: function (el, obj) {
 
     this.el = el;
     this.$el = $(el);
+    this.menuHeight = parseInt($('main').css("padding-top"));
 
     if(this.$el.find(".article__aside")) {
       ArticleRelatedContent.init(document.getElementById('article-related-content'));
     }
+
+    if(obj.type === "report") {
+      this.$header = this.$el.find(".article__header");
+      $(window).on('resize', _.bind(this.resizeMainImage, this));
+      this.resizeMainImage();
+    }
+  },
+
+  resizeMainImage: function functionName(e) {
+    var ratio = Math.min(0.5625, ($(window).height() - this.menuHeight)/$(window).width());
+    this.$header.css('padding-bottom', (ratio*100)+"%");
   }
 }
 
